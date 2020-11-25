d5ad16ecdeacca0df7197cb015e70fec064dc79d
sebauribeo <chefsebauribe@gmail.com>
2020-11-25 13:44:20 -0300

    commit agregar lista

---
+++ README.md
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+# desafio_nueva_lista
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+=======
+# Desafio-Agregar-Listas
+Agregar listas con vueJs
+>>>>>>> 6bb8a8680a501bfda668ed321c230265deddbe05
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,116 @@
+<template>
+
+  <div class="container">
+    <h1>Agrega Una Nueva Tarea</h1>
+    <div class="dates">
+      <input type="text" class="input" placeholder="Ingresa un Nombre" aria-label="nombre" aria-describedby="button-addon2" v-model="nombre">
+      <div class="input-group-append">
+        <button class="button" type="button" @click="mostrarNombre">Agregar</button>
+      </div>
+    </div>
+    
+
+    <section class="box" v-show="listaNombres.length > 0">
+      <div class="box-2">
+        <ul v-for="(nombre,index) in listaNombres" :key="index">
+          <li>{{nombre}}</li>
+        </ul>
+      </div>
+    </section>
+    
+  </div>
+  
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+   data() {
+        return {
+            nombre: '',
+            listaNombres: []
+        }
+    },
+    methods: {
+        mostrarNombre(){
+            if (this.nombre) {
+                this.listaNombres.push(this.nombre);
+                this.nombre = "";
+            } else {
+                console.log("no tiene nombre");
+            }
+        },
+    },
+}
+
+
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: #1d1d1d;
+}
+.box {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.box-2 {
+    position: relative;
+    margin: 50px;
+    width: 400px;
+    height: auto;
+    background: linear-gradient(0deg, #000000, #262626);
+    color: white;
+}
+.box-2:before, .box-2:after{
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background: linear-gradient(60deg, #fb0094, #0000ff, #00ff00, #fff000, #ff0000,#fb0094, #0000ff, #00ff00, #fff000, #ff0000);
+    background-size: 200%;
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    z-index: -1;
+    animation: animate1 20s linear infinite;
+}
+.box-2:after{
+filter: blur(20px);
+}
+@keyframes animate1{
+    0%{
+        background-position: 0 0;
+    }
+    50%{
+        background-position: 600% 0;
+    }
+    100%{
+        background-position: 0 0;
+    }
+}
+ul li{
+  text-align: justify;
+  font-weight: 50px;
+}
+h1 {
+  color: red;
+}
+.input {
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
+  text-align: center;
+}
+.button {
+  margin: 30px;
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  background: grey;
+  border: 3px solid red;
+}
+</style>
